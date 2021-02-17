cd9e1dd138277684df9c6477e9fe29ca513adeb2
Nevin Herring <nevin.herring@gmail.com>
2021-02-17 08:55:07 -0600

    catching overflow exceptions

--- Program.cs
+++ Program.cs
@@ -7,14 +7,25 @@ namespace CheckingForOverflow
     {
         static void Main(string[] args)
         {
-            int x = int.MaxValue - 1;
-            WriteLine($"Initial value: {x}");
-            x++;
-            WriteLine($"After incrementing: {x}");
-            x++;
-            WriteLine($"After incrementing: {x}");
-            x++;
-            WriteLine($"After incrementing: {x}");
+            try
+            {
+                checked
+                {
+                    int x = int.MaxValue - 1;
+                    WriteLine($"Initial value: {x}");
+                    x++;
+                    WriteLine($"After incrementing: {x}");
+                    x++;
+                    WriteLine($"After incrementing: {x}");
+                    x++;
+                    WriteLine($"After incrementing: {x}");
+                }
+            }
+            catch (OverflowException)
+            {
+                WriteLine("The code overflowed but I caught the exception");
+            }
+
         }
     }
 }
