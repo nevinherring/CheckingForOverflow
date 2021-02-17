094c6c979e248d7342dcd02c1ebf13484325d90c
Nevin Herring <nevin.herring@gmail.com>
2021-02-17 08:52:45 -0600

    checking for overflow

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+namespace CheckingForOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.MaxValue - 1;
+            WriteLine($"Initial value: {x}");
+            x++;
+            WriteLine($"After incrementing: {x}");
+            x++;
+            WriteLine($"After incrementing: {x}");
+            x++;
+            WriteLine($"After incrementing: {x}");
+        }
+    }
+}
